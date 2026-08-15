ee67faa6cb756980520694c5eaf82a6e3cf6729b
Rafael Antonio <rafaelantoniorodriguessilveira@gmail.com>
2026-08-15 11:15:47 -0300

    Adicionar arquivos de projeto.

---
+++ Alura Atividades/Program.cs
@@ -0,0 +1,10 @@
+﻿Console.Write("Digite um numero qualquer: ");
+int numero = int.Parse(Console.ReadLine()!);
+
+if( numero % 2 != 0)
+{
+    Console.WriteLine("O número digitado é impar");
+}else
+{
+    Console.WriteLine("O numero digitado e par");
+}
